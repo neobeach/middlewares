20ca68eb3d7956dc069ac1e93456d3102ec2e6b8
Glenn de Haan <glenn@dehaan.cloud>
2022-03-25 20:56:52 +0100

    [request-logger] Keep export the same across all middlewares even if there a no configuration options.

--- request-logger/index.js
+++ request-logger/index.js
@@ -4,23 +4,23 @@
 const {Logger} = require('@neobeach/core');
 
 /**
- * Log all incoming requests from express
+ * Log all incoming requests from Express
  *
- * @param req
- * @param res
- * @param next
+ * @return {function(*, *, *): void}
  */
-module.exports = (req, res, next) => {
-    Logger.info(`[EXPRESS][REQUEST](${req.method}): ${req.originalUrl}`);
-    Logger.debug(`[EXPRESS][REQUEST][${req.originalUrl}][HEADERS](${req.method}): ${JSON.stringify(req.headers)}`);
+module.exports = () => {
+    return (req, res, next) => {
+        Logger.info(`[EXPRESS][REQUEST](${req.method}): ${req.originalUrl}`);
+        Logger.debug(`[EXPRESS][REQUEST][${req.originalUrl}][HEADERS](${req.method}): ${JSON.stringify(req.headers)}`);
 
-    if (typeof req.cookies === "object") {
-        Logger.debug(`[EXPRESS][REQUEST][${req.originalUrl}][COOKIES](${req.method}): ${JSON.stringify(req.cookies)}`);
-    }
+        if (typeof req.cookies === "object") {
+            Logger.debug(`[EXPRESS][REQUEST][${req.originalUrl}][COOKIES](${req.method}): ${JSON.stringify(req.cookies)}`);
+        }
 
-    if (req.method === "POST" && typeof req.body === "object") {
-        Logger.debug(`[EXPRESS][REQUEST][${req.originalUrl}][DATA](${req.method}): ${JSON.stringify(req.body)}`);
-    }
+        if (req.method === "POST" && typeof req.body === "object") {
+            Logger.debug(`[EXPRESS][REQUEST][${req.originalUrl}][DATA](${req.method}): ${JSON.stringify(req.body)}`);
+        }
 
-    next();
+        next();
+    };
 };
