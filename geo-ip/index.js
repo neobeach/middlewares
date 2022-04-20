8d28d534b56bcdf28ee2f3de7dcf0b346428f134
roelvoordendag <rvoordendag@gmail.com>
2022-04-20 09:19:07 +0200

    Changed based on feedback on PR

--- geo-ip/index.js
+++ geo-ip/index.js
@@ -8,35 +8,37 @@ const expressGeoIp = require('express-geoip');
 * Checks if we should redirect based on GEO-IP
 *
 * @param {Object} routing
-* @param {Number} statusCode
 * @param {String} defaultCountryCode
+* @param {Number} statusCode
 * @param {Boolean} debug
 * @return {Function}
-* 
+*
 * @example
 * const globalMiddleware = [
-*    geoip({'nl': routingNLObject, 'en': routingENObject }, 301, 'nl', false)
+*    geoip({'nl': 'nl-NL'}, 'nl', 307, false)
 * ]
-* 
+*
 */
-module.exports = (routing, statusCode, defaultCountryCode, debug = false) => (req, res, next) => {
-    // Check if ip is in request and check if ip adress is know otherwise use default country code
-    const countryCode = req.ip ? expressGeoIp('unkown').getCountryCode(req.ip) == "unkown" ? defaultCountryCode : expressGeoIp('unkown').getCountryCode(req.ip) : defaultCountryCode;
+module.exports = (routing, defaultCountryCode, statusCode , debug = false) => {
+    return (req, res, next) => {
+        // Check if ip is in request and check if ip address is know otherwise use default country code
+        const countryCode = req.ip ? expressGeoIp('unknown').getCountryCode(req.ip) === "unknown" ? defaultCountryCode : expressGeoIp('unknown').getCountryCode(req.ip) : defaultCountryCode;
 
-    const urlWithoutParams = req.originalUrl.split("?")[0];
-
-    if (urlWithoutParams === "/") {
-        if (routing[countryCode.toLowerCase()]) {
-            if (debug) {
-                Logger.info(`[REDIRECT] Based on GeoIP: ${countryCode} -> ${routing[countryCode.toLowerCase()]}`);
+        const urlWithoutParams = req.originalUrl.split("?")[0];
+        console.log(countryCode);
+        if (urlWithoutParams === "/") {
+            if (routing[countryCode.toLowerCase()]) {
+                if (debug) {
+                    Logger.info(`[REDIRECT] Based on GeoIP: ${countryCode} -> ${routing[countryCode.toLowerCase()]}`);
+                }
+                res.redirect(statusCode, routing[countryCode.toLowerCase()]);
+                return;
+            } else {
+                res.redirect(statusCode, defaultCountryCode);
+                return;
             }
-            res.redirect(statusCode, routing[countryCode.toLowerCase()]);
-            return;
-        } else {
-            res.redirect(statusCode, defaultCountryCode);
-            return;
         }
+
+        next();
     }
-   
-    next();
 };
